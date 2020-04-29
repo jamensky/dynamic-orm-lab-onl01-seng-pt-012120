4b0ee80155d35bedf9df17f5e7a8662aea4b3f75
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-29 22:34:09 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
+
 class InteractiveRecord
 
   def self.table_name
@@ -24,5 +25,35 @@ class InteractiveRecord
     end
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|col_nm| col_nm == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ?", name)
+  end
+
+  def self.find_by(att)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE ? = ?", att.to_a[0][0].to_s, att.to_a[0][1])
+    binding.pry
+  end
 
 end
